41080aaee6b4a0a91a601201bf1099a0deb17251
Bernhard Reiter <ockham@raz.or.at>
2017-10-18 19:03:27 +0200

    Move trouleshoot section to dedicated file, drop styling

--- client/my-sites/site-settings/disconnect-site/index.jsx
+++ client/my-sites/site-settings/disconnect-site/index.jsx
@@ -6,7 +6,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { flowRight, get } from 'lodash';
 import { localize } from 'i18n-calypso';
-import Gridicon from 'gridicons';
 
 /**
  * Internal dependencies
@@ -15,13 +14,12 @@ import DisconnectSurvey from './disconnect-survey';
 import MissingFeature from './missing-feature';
 import TooDifficult from './too-difficult';
 import TooExpensive from './too-expensive';
+import Troubleshoot from './troubleshoot';
 import DocumentHead from 'components/data/document-head';
 import FormattedHeader from 'components/formatted-header';
 import NavigationLink from 'components/wizard/navigation-link';
 import { getSelectedSiteSlug } from 'state/ui/selectors';
-import CompactCard from 'components/card/compact';
 import Main from 'components/main';
-import SectionHeader from 'components/section-header';
 import Placeholder from 'my-sites/site-settings/placeholder';
 import redirectNonJetpack from 'my-sites/site-settings/redirect-non-jetpack';
 
@@ -59,20 +57,7 @@ const DisconnectSite = ( { reason, siteSlug, translate } ) => {
 					<NavigationLink href={ backHref } direction="back" />
 					<NavigationLink href={ confirmHref } direction="forward" />
 				</div>
-				<div className="disconnect-site__troubleshooting">
-					<SectionHeader label={ translate( 'Having problems with your connection?' ) } />
-					<CompactCard
-						href={ 'https://jetpack.com/support/debug/?url=' + siteSlug }
-						target="_blank"
-					>
-						<Gridicon icon="refresh" />
-						{ translate( 'Diagnose a connection problem' ) }
-					</CompactCard>
-					<CompactCard>
-						<Gridicon icon="chat" />
-						{ translate( 'Get help from our Happiness Engineers' ) }
-					</CompactCard>
-				</div>
+				<Troubleshoot />
 			</Main>
 		</div>
 	);
